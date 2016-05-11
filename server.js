8415482e12b82cef12739867dbe958d58df88484
Dominik Lubański <dominik.lubanski@gmail.com>
2016-05-11 11:28:02 +0200

    Basic auth

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
+require('dotenv').config(); // Load local environment
+
 const express = require('express');
 const app = express();
+const basicAuth = require('basic-auth');
 
 app.set('port', (process.env.PORT));
+
+app.use((req, res, next) => {
+  function unauthorized(authRes) {
+    authRes.set('WWW-Authenticate', 'Basic realm=Authorization Required');
+    return authRes.sendStatus(401);
+  }
+
+  const user = basicAuth(req);
+
+  if (!user || !user.name || !user.pass) {
+    return unauthorized(res);
+  }
+
+  if (user.name === process.env.BASIC_AUTH_USER && user.pass === process.env.BASIC_AUTH_PASS) {
+    return next();
+  }
+
+  return unauthorized(res);
+});
+
 app.use(express.static(`${process.cwd()}/build`));
 
 app.listen(app.get('port'), () => {
